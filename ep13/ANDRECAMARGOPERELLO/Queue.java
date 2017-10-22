760ce594153ae76e9846b5e16893a0860e83d03c
Andre C Perello <andrecperello@gmail.com>
2017-10-22 11:40:05 -0200

    First implementation - MergeSort adaptation.

--- ep13/ANDRECAMARGOPERELLO/Queue.java
+++ ep13/ANDRECAMARGOPERELLO/Queue.java
@@ -10,6 +10,25 @@
  *
  ******************************************************************************/
 
+/******************************************************************************
+ *
+ * MAC0121 - Algoritmos e Estruturas de Dados I
+ * Aluno: Andre Camargo Perello
+ * Numero USP: 9912403
+ * Tarefa: Queue shuffle.
+ * Data: 22/10/2017
+ *
+ *
+ *
+ * DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESTE PROGRAMA.  TODAS AS
+ * PARTES DO PROGRAMA, EXCETO AS QUE SÃO BASEADAS EM MATERIAL FORNECIDO
+ * PELO PROFESSOR OU COPIADAS DO LIVRO OU DAS BIBLIOTECAS DE SEDGEWICK & WAYNE,
+ * FORAM DESENVOLVIDAS POR MIM.  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS
+ * AS CÓPIAS DESTE PROGRAMA E QUE NÃO DISTRIBUÍ NEM FACILITEI A DISTRIBUIÇÃO
+ * DE CÓPIAS DESTA PROGRAMA.
+ *
+ ******************************************************************************/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -43,6 +62,16 @@ public class Queue<Item> implements Iterable<Item> {
     private class Node {
         private Item item;
         private Node next;
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+            Node tmp = this;
+            while (tmp != null) {
+                s.append(tmp.item);
+                s.append(' ');
+                tmp = tmp.next;
+            }
+            return s.toString();
+        }
     }
 
     /**
@@ -50,7 +79,7 @@ public class Queue<Item> implements Iterable<Item> {
      */
     public Queue() {
         first = null;
-        last  = null;
+        last = null;
         n = 0;
     }
 
@@ -69,7 +98,7 @@ public class Queue<Item> implements Iterable<Item> {
      * @return the number of items in this queue
      */
     public int size() {
-        return n;     
+        return n;
     }
 
     /**
@@ -78,7 +107,7 @@ public class Queue<Item> implements Iterable<Item> {
      * @return the number of items in this queue
      */
     public int length() {
-        return n;     
+        return n;
     }
 
     /**
@@ -92,7 +121,7 @@ public class Queue<Item> implements Iterable<Item> {
         return first.item;
     }
 
-   /**
+    /**
      * Add the item to the queue.
      */
     public void enqueue(Item item) {
@@ -101,7 +130,7 @@ public class Queue<Item> implements Iterable<Item> {
         last.item = item;
         last.next = null;
         if (isEmpty()) first = last;
-        else           oldlast.next = last;
+        else oldlast.next = last;
         n++;
     }
 
@@ -132,44 +161,115 @@ public class Queue<Item> implements Iterable<Item> {
             s.append(' ');
         }
         return s.toString();
-    } 
- 
+    }
+
 
     /**
      * Returns an iterator that iterates over the items in this queue in FIFO order.
      *
      * @return an iterator that iterates over the items in this queue in FIFO order
      */
-    public Iterator<Item> iterator()  {
-        return new ListIterator();  
+    public Iterator<Item> iterator() {
+        return new ListIterator();
     }
 
     // an iterator, doesn't implement remove() since it's optional
     private class ListIterator implements Iterator<Item> {
         private Node current = first;
 
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
+
+    //**********************************************************************
+
+
+    private Node getMiddle(Node head) {
+        if(head == null) return head;
+        Node fastPointer = head.next;
+        Node slowPoitner = head;
+
+        //When fastPointer hits the end, slow pointer will be at the middle, given that slowPointer moves half the "speed" of fastPointer
+        while(fastPointer != null) {
+            fastPointer = fastPointer.next;
+            if(fastPointer != null) {
+                slowPoitner = slowPoitner.next;
+                fastPointer = fastPointer.next;
+            }
+        }
+        return slowPoitner;
+    }
+
+    //Para cada chamada de merge, randomizamos a escolha, ao inves de ordenar. Por utiizar uma estrategia de divide and conquer, todos os nodes tem a mesma probabilidade de aparecer
+    //nem uma dada posicao. Isso se da pois a cada merge, os nodes tem probabilidade de assumir uma dada posicao.
+    //. Utilizamos a ideia de moedas para facilitar a interpretacao. Ou seja,
+    //Um Node x_i assume a posicao j se x_i = 1 ( ou seja cara) com Pr(x_i = 1) = 1/2 e Pr(x_i = 0) = 1/2.
+    private Node merge(Node left, Node right) {
+        Node result = null;
+
+        if(left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        if(StdRandom.uniform(10) > 5) {
+            result = left;
+            result.next = merge(left.next, right);
+        }
+        else {
+            result = right;
+            result.next = merge(left, right.next);
+        }
+        return result;
+    }
+
+
+    //Corpo do "mergeShuffle". Utiliza uma estrategia de divide and conquer para dividir a lista e aleatorizar cada merge.
+    //Como isto eh um algoritmo de mergeSort com a funcao merge levemente mudada, ele tera a mesma complexidade O(nlogn) e espaco O(logn).
+    private Node shuffleHelper(Node head) {
+        if(head == null || head.next == null) return head;
+        Node mid = getMiddle(head);
+        Node nextMid = mid.next;
+        mid.next = null;
+        Node left = shuffleHelper(head);
+        Node right = shuffleHelper(nextMid);
+
+        return merge(left, right);
+
+    }
+
+    //Funcao que chama o mergesort. Um wrapper.
+    public void shuffle() {
+
+        this.first = shuffleHelper(this.first);
+    }
+
 
 
     /**
      * Unit tests the {@code Queue} data type.
      */
     public static void main(String[] args) {
-        Queue<String> queue = new Queue<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) queue.enqueue(item);
-            else if (!queue.isEmpty()) StdOut.print(queue.dequeue() + " ");
+        Queue<Integer> queue = new Queue<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            queue.enqueue(i);
         }
-        StdOut.println("(" + queue.size() + " left on queue)");
+        queue.shuffle();
+        System.out.println(queue);
+
     }
 }
